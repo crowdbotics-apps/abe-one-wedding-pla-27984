6385c41f9f3fb55777fa1f64f5aabcaeeaf2e26d
Crowdbotics <team@crowdbotics.com>
2021-06-15 03:56:05 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/abe_one_wedding_pla_27984/MainActivity.java
@@ -0,0 +1,15 @@
+package com.abe_one_wedding_pla_27984;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "abe_one_wedding_pla_27984";
+  }
+}
